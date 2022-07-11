e14acd0e93bdc8cab1018381e0c11ae2515375d7
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-07-11 14:05:37 +0200

    esercizio ok

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
 const root = new Vue({
     el: "#root",
     data: {
-        messaggio1: "",
-        messaggio2: "",
-        fotoSrc: ""
+        messaggio1: "Ciao Mondo",
+        messaggio2: "Hello World",
+        fotoSrc: "img/184310709-24e023da-f854-4eaa-986f-4b270b2a59b0.jpeg"
     },
     methods: {}
 })
